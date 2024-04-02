b6617af6bb118aff7ba5f00f07a2b7f617d80804
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-01 21:54:48 -0300

    I reduce. deleting the lib that are unused to create a more simple repo for new devs.

--- lib/getArweaveLink.tsx
+++
@@ -1,7 +0,0 @@
-const getArweaveLink = (arweaveString: string) => {
-  const url = arweaveString.replace('ar://', 'https://arweave.net/');
-
-  return url;
-};
-
-export default getArweaveLink;
--- lib/getIPFS.tsx
+++
@@ -1,2 +0,0 @@
-export const getIpfsLink = (hash: string) =>
-  hash?.indexOf?.('ipfs://') > -1 ? hash.replace('ipfs://', 'https://nftstorage.link/ipfs/') : hash;
